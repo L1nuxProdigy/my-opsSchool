ed97e43ea1f99d2ee3a5db016d7dad9f52e55208
Nadav <nadavops@protonmail.com>
2018-12-14 16:18:54 +0200

    tags to vpc creation

--- boto3/session1/vpc.py
+++ boto3/session1/vpc.py
@@ -4,3 +4,5 @@ import argparse
 
 ec2 = boto3.resource('ec2')
 vpc = ec2.create_vpc(CidrBlock='172.16.0.0/16',InstanceTenancy='default')
+vpc.wait_until_exists
+tag = vpc.create_tags(Tags=[{'Key': 'Name','Value':'VPC_opsschool1'}])
