a2fb5e43f9bac1d30997baca340616de977b06fc
Nadav <nadavops@protonmail.com>
2018-12-14 17:42:51 +0200

    adding to delete vpc, and adding a sketch file

--- boto3/session1/delete_vpc.py
+++ boto3/session1/delete_vpc.py
@@ -2,6 +2,17 @@
 import boto3
 import argparse
 
+# defining resources
+ec2 = boto3.resource('ec2')
 client = boto3.client('ec2')
+
 vpc_id = client.describe_vpcs()['Vpcs'][0]['VpcId']
+vpc = ec2.Vpc(vpc_id)
+
+if len(client.describe_internet_gateways()['InternetGateways']) > 0:
+    vpc_id = client.describe_internet_gateways()['InternetGateways'][0]['Attachments'][0]['VpcId']
+    internet_gateway_id = client.describe_internet_gateways()['InternetGateways'][0]['InternetGatewayId']
+    vpc = ec2.Vpc(vpc_id)
+    vpc.detach_internet_gateway(InternetGatewayId=internet_gateway_id)
+
 client.delete_vpc(VpcId=vpc_id)
---
+++ boto3/session1/stupidfile.py
@@ -0,0 +1,132 @@
+{
+    'Vpcs': [{
+        'CidrBlock': '172.16.0.0/16',
+        'DhcpOptionsId': 'dopt-07c1acc6fa8d6e256',
+        'State': 'available',
+        'VpcId': 'vpc-0c2364dc66737e2c8',
+        'OwnerId': '920354577513',
+        'InstanceTenancy': 'default',
+        'CidrBlockAssociationSet': [{
+            'AssociationId': 'vpc-cidr-assoc-01d3ea4c620884f94',
+            'CidrBlock': '172.16.0.0/16',
+            'CidrBlockState': {
+                'State': 'associated'
+            }
+        }],
+        'IsDefault': False,
+        'Tags': [{
+            'Key': 'Name',
+            'Value': 'VPC_opsschool1'
+        }]
+    }, {
+        'CidrBlock': '172.16.0.0/16',
+        'DhcpOptionsId': 'dopt-07c1acc6fa8d6e256',
+        'State': 'available',
+        'VpcId': 'vpc-06510617ee6fb9fa2',
+        'OwnerId': '920354577513',
+        'InstanceTenancy': 'default',
+        'CidrBlockAssociationSet': [{
+            'AssociationId': 'vpc-cidr-assoc-0d7916932c31be378',
+            'CidrBlock': '172.16.0.0/16',
+            'CidrBlockState': {
+                'State': 'associated'
+            }
+        }],
+        'IsDefault': False,
+        'Tags': [{
+            'Key': 'Name',
+            'Value': 'VPC_opsschool1'
+        }]
+    }],
+    'ResponseMetadata': {
+        'RequestId': 'd90d455d-d164-49c5-a120-3ef8be65ed27',
+        'HTTPStatusCode': 200,
+        'HTTPHeaders': {
+            'content-type': 'text/xml;charset=UTF-8',
+            'content-length': '2101',
+            'vary': 'Accept-Encoding',
+            'date': 'Fri, 14 Dec 2018 14:33:37 GMT',
+            'server': 'AmazonEC2'
+        },
+        'RetryAttempts': 0
+    }
+}
+
+
+import boto3
+
+# defining resources
+ec2 = boto3.resource('ec2')
+client = boto3.client('ec2')
+
+# defining a vpc resource and picking a vpc
+print("The amount of existing VPC's: ",len(client.describe_vpcs()['Vpcs']))
+pick_vpc = int(input("Enter an Int to pick a VPC (starting from 0): "))
+vpc_id = client.describe_vpcs()['Vpcs'][pick_vpc]['VpcId']
+vpc = ec2.Vpc(vpc_id)
+
+# creating an IG and picking one
+client.create_internet_gateway()
+print("The amount of existing IG's: ",len(client.describe_internet_gateways()['InternetGateways']))
+pick_IG = int(input("Enter an Int to pick a IG (starting from 0): "))
+internet_gateway_id = client.describe_internet_gateways()['InternetGateways'][pick_IG]['InternetGatewayId']
+
+# attach the IG to the VPC
+vpc.attach_internet_gateway(InternetGatewayId=internet_gateway_id)
+
+
+
+{
+    'Vpcs': [{
+        'CidrBlock': '172.16.0.0/16',
+        'DhcpOptionsId': 'dopt-07c1acc6fa8d6e256',
+        'State': 'available',
+        'VpcId': 'vpc-076c0ccb1ea747264',
+        'OwnerId': '920354577513',
+        'InstanceTenancy': 'default',
+        'CidrBlockAssociationSet': [{
+            'AssociationId': 'vpc-cidr-assoc-0508a6e9a5edf0ea1',
+            'CidrBlock': '172.16.0.0/16',
+            'CidrBlockState': {
+                'State': 'associated'
+            }
+        }],
+        'IsDefault': False,
+        'Tags': [{
+            'Key': 'Name',
+            'Value': 'VPC_opsschool1'
+        }]
+    }],
+    'ResponseMetadata': {
+        'RequestId': '1f87e71d-0a1a-4ce9-9fae-4ce0246baba0',
+        'HTTPStatusCode': 200,
+        'HTTPHeaders': {
+            'content-type': 'text/xml;charset=UTF-8',
+            'content-length': '1163',
+            'date': 'Fri, 14 Dec 2018 14:41:56 GMT',
+            'server': 'AmazonEC2'
+        },
+        'RetryAttempts': 0
+    }
+}
+
+
+{
+    'InternetGateways': [{
+        'Attachments': [],
+        'InternetGatewayId': 'igw-055552a8473e2a0eb',
+        'OwnerId': '920354577513',
+        'Tags': []
+    }],
+    'ResponseMetadata': {
+        'RequestId': '352608ad-94a1-4b0e-83ff-612c076a4b9d',
+        'HTTPStatusCode': 200,
+        'HTTPHeaders': {
+            'content-type': 'text/xml;charset=UTF-8',
+            'content-length': '472',
+            'date': 'Fri, 14 Dec 2018 15:15:07 GMT',
+            'server': 'AmazonEC2'
+        },
+        'RetryAttempts': 0
+    }
+}
