6fd362afb2a0f02a89c091806c25b056b84fb5dd
Nadav <nadavops@protonmail.com>
2018-12-18 23:58:59 +0200

    changes to subnets and routes

--- boto3/session1/subnets_routes_nat.py
+++ boto3/session1/subnets_routes_nat.py
@@ -9,9 +9,11 @@ waiter = client.get_waiter('nat_gateway_available')
 #Creating 2 Subnets in a VPC
 vpc_id = client.describe_vpcs()['Vpcs'][0]['VpcId']
 private_subnet = ec2.create_subnet(AvailabilityZoneId='euw3-az3',CidrBlock='10.21.1.0/24',VpcId=vpc_id)
+private_subnet_id = client.describe_subnets(Filters=[{'Name': 'cidr-block','Values': ['10.21.1.0/24']}])['Subnets'][0]['SubnetId']
 public_subnet = ec2.create_subnet(AvailabilityZoneId='euw3-az3',CidrBlock='10.21.2.0/24',VpcId=vpc_id)
 private_subnet.create_tags(Tags=[{'Key': 'Name','Value': 'Private_1c'}])
 public_subnet.create_tags(Tags=[{'Key': 'Name','Value': 'Public_1c'}])
+public_subnet_id = client.describe_subnets(Filters=[{'Name': 'cidr-block','Values': ['10.21.2.0/24']}])['Subnets'][0]['SubnetId']
 
 #Nat and elastic IP
 client.allocate_address()
@@ -26,6 +28,13 @@ nat_gateway_id = client.describe_nat_gateways(Filters=[{'Name': 'state','Values'
 public_route_table = client.create_route_table(VpcId=vpc_id)
 public_route_table_id = public_route_table['RouteTable']['RouteTableId']
 client.create_route(DestinationCidrBlock='0.0.0.0/0',GatewayId=internet_gateway_id,RouteTableId=public_route_table_id)
+client.associate_route_table(RouteTableId=public_route_table_id,SubnetId=public_subnet_id)
+public_route_table_resource = ec2.RouteTable(public_route_table_id)
+public_route_table_resource.create_tags(Tags=[{'Key': 'Name','Value': 'Public_1c'}])
+
 private_route_table = client.create_route_table(VpcId=vpc_id)
 private_route_table_id = private_route_table['RouteTable']['RouteTableId']
 client.create_route(DestinationCidrBlock='0.0.0.0/0',GatewayId=nat_gateway_id,RouteTableId=private_route_table_id)
+client.associate_route_table(RouteTableId=public_route_table_id,SubnetId=private_subnet_id)
+private_route_table_resource = ec2.RouteTable(private_route_table_id)
+private_route_table_resource.create_tags(Tags=[{'Key': 'Name','Value': 'Private_1c'}])
