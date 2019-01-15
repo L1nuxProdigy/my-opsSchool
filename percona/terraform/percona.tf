c6ead7bfdcc52ec17a249187e8f7549b1b95cb94
nadav <nadavmailspam@gmail.com>
2019-01-15 23:55:09 +0200

    percona

---
+++ percona/terraform/percona.tf
@@ -0,0 +1,50 @@
+##################################################################################
+# VARIABLES
+##################################################################################
+
+### Connection Vars ###
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "private_key_path" {}
+variable "key_name" {
+  default = "New"
+}
+
+### Machines Configurations Scripts ###
+variable "percona_script_path" {}
+
+
+
+
+##################################################################################
+# PROVIDERS
+##################################################################################
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "eu-central-1"
+}
+
+##################################################################################
+# EC2 Resources
+##################################################################################
+
+resource "aws_instance" "percona" {
+	ami           = "ami-c86c3f23"
+	instance_type = "t2.micro"
+	key_name        = "${var.key_name}"
+	vpc_security_group_ids = ["sg-02e7cd2c6090514d4"]
+
+	connection {
+		user        = "ec2-user"
+		private_key = "${file(var.private_key_path)}"
+	}
+	
+	tags = {
+	Name = "Percona_by_Terraform"
+    }
+	
+	user_data = 
+	
+}
